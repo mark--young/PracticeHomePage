c29e4b96d5d1550e1682fdc403097e32d921e268
Roger1492 <roger1487177144@gmail.com>
2016-08-05 21:08:44 +0800

    add some

---
+++ Ranger/js/nav.js
@@ -0,0 +1,7 @@
+var menubar = document.getElementById("menubar");
+var menuItems = menubar.getElementsByClassName("brackets");
+for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].onclick = function(){
+        
+    }
+}
