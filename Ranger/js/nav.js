20213be608661b3ab3f23592c4ab96ca2ecb65cc
Roger1492 <roger1487177144@gmail.com>
2016-08-06 23:26:39 +0800

    add section 1

--- Ranger/js/nav.js
+++ Ranger/js/nav.js
@@ -2,6 +2,6 @@ var menubar = document.getElementById("menubar");
 var menuItems = menubar.getElementsByClassName("brackets");
 for (var i = 0; i < menuItems.length; i++) {
     menuItems[i].onclick = function(){
-        
+        menuItems[i].addClass = "showB";
     }
 }
