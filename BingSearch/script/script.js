c71ab255652690cf481f1dd3777ae560de9fb8e0
Roger Shen <rogershen1492@gmail.com>
2016-09-09 16:37:31 +0800

    Bing search
    这是模仿的Bing搜索首页，还不完善。有待改进，在分析页面结构时分析错了，导致后面编码困难。以后要注意，在分析页面结构时一定要把结构分析正确，这样编码就会相对容易些。

---
+++ BingSearch/script/script.js
@@ -0,0 +1,15 @@
+// $(document).ready(function($) {
+// 	$("#number").mouseover(function() {
+// 		$("#wechat").css({
+// 			transform: 'translateX(-20px)',
+// 			opacity: '1'
+// 			// transition = 'all .2s'
+// 		});
+// 	});
+// 	$("#number").mouseout(function(){
+// 		$("#wechat").css({
+// 			transform: 'translateX(40px)',
+// 			opacity: '0'
+// 		});
+// 	});
+// });
